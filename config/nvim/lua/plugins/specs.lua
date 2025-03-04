1d7ccaace4a38bb357bfab852d66adc4e5a4e991
Vasily Negrebetskiy <vasilynegrebetskiy@gmail.com>
2025-03-04 06:44:46 +0400

    Added mypy

--- config/nvim/lua/plugins/specs.lua
+++ config/nvim/lua/plugins/specs.lua
@@ -66,7 +66,13 @@ return {
         "neovim/nvim-lspconfig",
         tag = "v0.1.8",
         config = function()
-            require("lspconfig").pylsp.setup {}
+            require("lspconfig").pylsp.setup {
+                pylsp = {
+                    pylsp_mypy = {
+                        enabled = true,
+                    },
+                }
+            }
         end
     },
     {
