d76fdfa4b096251348e78fed3a5a5b2c87df530c
Vasily Negrebetskiy <vasilynegrebetskiy@gmail.com>
2025-03-12 02:17:33 +0400

    pylsp plugins tinkering

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -1,12 +1,12 @@
 vim.opt.encoding="utf-8"
 vim.opt.number = true
 vim.opt.wrap = false
-vim.opt.clipboard = 'unnamedplus'
+vim.opt.clipboard = "unnamedplus"
 vim.opt.cursorline = true
-vim.opt.cursorlineopt = 'number'
+vim.opt.cursorlineopt = "number"
 vim.opt.ignorecase = true
 vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = "yes"
 
 -- Indents
 vim.opt.tabstop = 4
@@ -14,6 +14,8 @@ vim.opt.smartindent = true
 vim.opt.shiftwidth = 4
 vim.opt.expandtab = true
 vim.opt.softtabstop = 4
+
+-- vim.lsp.set_log_level("debug")
 
 -- Make sure to setup `mapleader` and `maplocalleader` before
 -- loading lazy.nvim so that mappings are correct.
@@ -26,27 +28,31 @@ require("config.lazy")
 vim.cmd("Neotree")
 
 -- This configuration is set only when any LSP is attached to the buffer
-vim.api.nvim_create_autocmd('LspAttach', {
-  group = vim.api.nvim_create_augroup('UserLspConfig', {}),
-  callback = function(ev)
-    -- Buffer local mappings
-    local opts = { buffer = ev.buf }
-    
-    -- Go to the definition of the symbol (return with CTRL-I)
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
-
-    -- Show a hover window with symbol's docs
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
-     
-    -- Rename the symbol inside current buffer 
-    vim.keymap.set('n', '<space>rn', vim.lsp.buf.rename, opts)
-    
-    -- Show symbol usage in the project
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, opts)
-
-    vim.keymap.set('n', 'gs', vim.lsp.buf.signature_help, opts)
- 
-    -- Show diagnostics message
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float, opts)
-  end
+vim.api.nvim_create_autocmd("LspAttach", {
+    group = vim.api.nvim_create_augroup("UserLspConfig", {}),
+    callback = function(ev)
+        -- Buffer local mappings
+        local opts = { buffer = ev.buf }
+        
+        -- Go to the definition of the symbol (return with CTRL-I)
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        
+        -- Show a hover window with symbol"s docs
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+         
+        -- Rename the symbol inside current buffer 
+        vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
+        
+        -- Show symbol usage in the project
+        vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
+        
+        -- Show signature help
+        vim.keymap.set("n", "gs", vim.lsp.buf.signature_help, opts)
+        
+        -- Show diagnostics message
+        vim.keymap.set("n", "<space>e", vim.diagnostic.open_float, opts)
+        
+        -- Format the whole buffer (in normal mode) or the selection (in visual mode) 
+        vim.keymap.set({"n", "v"}, "<space>f", vim.lsp.buf.format, opts)
+    end
 })
--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -2,11 +2,28 @@ return {
     "neovim/nvim-lspconfig",
     tag = "v0.1.8",
     config = function()
-        require("lspconfig").pylsp.setup {
-            pylsp = {
-                pylsp_mypy = {
-                    enabled = true,
-                },
+        local lspconfig = require("lspconfig")
+        
+        lspconfig.util.on_setup = lspconfig.util.add_hook_before(
+            lspconfig.util.on_setup,
+            function(config)
+                if config.name == "pylsp" then
+                    config.cmd = {"pylsp", "--log-file", "/home/kmicic/pylsp.log", "-vv"}
+                end
+            end
+        )
+
+        lspconfig.pylsp.setup {
+            settings = {
+                pylsp = {
+                    plugins = {
+                        -- enabled by default but still
+                        pylsp_mypy = {enabled = true},
+                        ruff = {enabled = true},
+                        -- my experimental plugin
+                        -- pylsp_skill = {enabled = true},
+                    }
+                }
             }
         }
     end
