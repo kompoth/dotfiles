c581e0a34827ff642d9a724ffa30aafac13c1472
Vasily Negrebetskiy <vasilynegrebetskiy@gmail.com>
2025-04-03 19:54:19 +0400

    Trying new setup

--- config/nvim/lua/plugins/lspconfig.lua
+++ config/nvim/lua/plugins/lspconfig.lua
@@ -1,19 +1,29 @@
 return {
     "neovim/nvim-lspconfig",
-    tag = "v0.1.8",
     config = function()
         local lspconfig = require("lspconfig")
         
+        lspconfig.ruff.setup{}
+
         lspconfig.pylsp.setup {
             cmd = {"pylsp", "-vv", "--log-file", "/tmp/pylsp.log"},
             settings = {
                 pylsp = {
                     plugins = {
-                        -- enabled by default but still
+                        -- For type checking 
                         pylsp_mypy = {enabled = true},
-                        ruff = {enabled = true},
-                        -- my experimental plugin
-                        starkiller = {enabled = true},
+
+                        -- Keeping ruff-pylsp to shutdowm default plugins
+                        ruff = {enabled = false},
+                        
+                        -- My experimental plugin
+                        starkiller = {
+                            enabled = true,
+                            aliases = {
+                                numpy = "np",
+                                ["matplotlib.pyplot"] = "plt",
+                            }
+                        }
                     }
                 }
             }
